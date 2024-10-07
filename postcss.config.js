46f138f34f105802feea148ee4fec467be7c4906
etamisie <32704830+ElsaTam@users.noreply.github.com>
2024-10-07 12:29:13 -0400

    PostCSS: remove selectors fas-

---
+++ postcss.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    plugins: [
+        require('postcss-remove-selectors')({
+            selectors: [/.*\.fas-(?!doodle-icons)/]
+        }),
+    ],
+ };
+
+ /*
+var fs = require('fs');
+var postcss = require('postcss')
+var remove = require('postcss-remove-selectors')
+
+const data = fs.readFileSync('css/cssclasses/sci-fi.css', { encoding: 'utf8' });
+const css = data.toString();
+
+//var options = {rulesToRemove: ['.fas-h1-display'], map: false} // css-byebye
+//var options = {'.fas-h1-display': '*'} // postcss-remove-rules
+//var options = ['.fas-h1-display'] // postcss-remove-classes
+var options = { selectors: ['.fas-', 'body.fas-'] } // postcss-remove-selectors
+
+console.log(options)
+var result = postcss(remove(options)).process(css).css
+console.log(typeof(result))
+//fs.writeFileSync('postcss/cssclasses/sci-fi.css', result);
+
+
+module.exports = {
+    plugins: {
+        remove
+    },
+}*/
